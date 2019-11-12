fd529104658d9527f9cc7f78ba06f7cc811c47a3
Hanwen Zhao <zhaolhw@gmail.com>
2019-11-12 14:41:22 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ultrasound_live.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ultrasound_live.dir/main.cpp.o"
+  "ultrasound_live"
+  "ultrasound_live.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ultrasound_live.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
